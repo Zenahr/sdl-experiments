63f93369f01fcbbc3b69a01ca77cf932d8966285
Zenahr Barzani <zenmatica@gmail.com>
2020-04-08 03:40:55 +0200

    added basic fatal-error handling

--- SDL-Projects/main.cpp
+++ SDL-Projects/main.cpp
@@ -1,5 +1,15 @@
 #include "Commons.h"
 #include "MainGame.h"
+#include <string>
+
+void fatalError(std::string error)
+{
+	std::cout << error << std::endl;
+	std::cout << "Enter any key to quit...";
+	std::cin.get();
+	SDL_Quit();
+
+}
 
 int main(int argc, char *argv[])
 {
