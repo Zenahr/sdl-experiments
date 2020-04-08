a864eaef3ff50ba98b22dbf22a3e80b2b3639dc2
Zenahr Barzani <zenmatica@gmail.com>
2020-04-08 02:57:37 +0200

    created openGL window

--- SDL-Projects/MainGame.cpp
+++ SDL-Projects/MainGame.cpp
@@ -1,11 +1,27 @@
 #include "MainGame.h"
 
+// Constructor
 MainGame::MainGame()
+{
+	_window = nullptr;
+	_screenWidth = 1024;
+	_screenHeight = 768;
+}
+
+// Desctructor
+MainGame::~MainGame()
 {
 
 }
 
-MainGame::~MainGame()
-{
 
+void MainGame::run()
+{
+	initSystems();
+}
+
+void MainGame::initSystems()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	_window = SDL_CreateWindow("Zenahr Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_OPENGL);
 }
--- SDL-Projects/MainGame.h
+++ SDL-Projects/MainGame.h
@@ -1,8 +1,18 @@
 #pragma once
+#include "SDL.h"
+
 class MainGame
 {
 public:
 	MainGame();
 	~MainGame();
+
+	void run();
+	void initSystems();
+
+private:
+	SDL_Window* _window;
+	int _screenWidth;
+	int _screenHeight;
 };
 
